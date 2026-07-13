e58674e9031a16d5010efd1e4f0c62ad92e5165f
Anna Bystrova <annbystr@gmail.com>
2026-07-13 11:51:09 +0300

    Настройка базовой фикстуры Android драйвера

---
+++ tests/conftest.py
@@ -0,0 +1,35 @@
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+import pytest
+
+
+APPIUM_PORT = 4723
+APPIUM_HOST = '127.0.0.1'
+
+@pytest.fixture(scope='session')
+def create_android_driver():
+
+    options = UiAutomator2Options()
+
+    # Необходимые capabilities
+    options.set_platform_name("Android")
+    options.set_device_name("Android") #Имя эмулятора
+    options.set_app_package("ru.iteco.fmhandroid")
+    options.set_app_activity(".ui.AppActivity")
+
+
+    options.set_no_reset(True)  # Между тестами не будет перезапуска приложения
+    options.set_auto_grant_permissions(True)
+
+    driver = webdriver.Remote(
+        command_executor=f'http://{APPIUM_HOST}:{APPIUM_PORT}',
+        options=options
+    )
+
+    yield driver
+
+    driver.quit()
+
+
+
+
