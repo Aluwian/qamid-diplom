3232242ac628b2c1bcc2bd48feeee4cc12fafd18
Anna Bystrova <annbystr@gmail.com>
2026-07-15 11:45:19 +0300

    Проектировани класса для проверки страницы авторизации

---
+++ tests/pages/login_page.py
@@ -0,0 +1,21 @@
+from appium.webdriver.common.appiumby import AppiumBy
+from pages.base_page import BasePage
+
+
+class LoginPage(BasePage):
+    LOGIN_FIELD = (
+        AppiumBy.ANDROID_UIAUTOMATOR,
+        'new UiSelector().text("Логин")'
+    )
+    PASSWORD_FIELD = (
+        AppiumBy.ANDROID_UIAUTOMATOR,
+        'new UiSelector().text("Пароль")'
+    )
+    LOGIN_BUTTON = (AppiumBy.ID, "ru.iteco.fmhandroid:id/enter_button")
+    TOAST_MESSAGE = (AppiumBy.XPATH, "//android.widget.Toast")
+
+    def login(self, login, password):
+        # Авторизация
+        self.send_keys(*self.LOGIN_FIELD, login)
+        self.send_keys(*self.PASSWORD_FIELD, password)
+        self.click(*self.LOGIN_BUTTON)
